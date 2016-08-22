dd5f7644c20fd7ab3c6202dfa8005020a10574ae
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-08-22 17:49:51 +0100

    Add the messages when enter is pressed with some JavaScript

---
+++ auto-empathie.js
@@ -0,0 +1,49 @@
+(function ()
+ {
+   function doubleNumber(n)
+   {
+     if (n < 10)
+       return "0" + n;
+     else
+       return "" + n;
+   }
+
+   function createTimeString(d)
+   {
+     return doubleNumber(d.getHours()) + ":" + doubleNumber(d.getMinutes());
+   }
+
+   function setup()
+   {
+     var inputBox = $("#inputbox");
+     inputBox.keypress(function(e) {
+       if (e.which != 13)
+         return;
+
+       e.preventDefault();
+       var text = inputBox.text();
+
+       if (!(/^\S/).test(text))
+         return;
+
+       var bubble = document.createElement("div");
+       bubble.className = "bubble you";
+       bubble.appendChild(document.createTextNode(text));
+
+       var timestamp = document.createElement("div");
+       var timestampText = createTimeString(new Date());
+       timestamp.appendChild(document.createTextNode(timestampText));
+       timestamp.className = "timestamp";
+       bubble.appendChild(timestamp);
+
+       $("#speech-container").append(bubble);
+
+       inputBox.empty();
+
+       var container = $("#speech-container")[0];
+       container.scrollTop = container.scrollHeight;
+     });
+   }
+
+   $(document.body).ready(setup);
+ })();
