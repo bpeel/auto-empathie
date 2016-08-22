127d7eb86413decb2a175e57c482081d0688341c
Neil Roberts <bpeeluk@yahoo.co.uk>
2016-08-22 17:51:50 +0100

    Move the message handling to a separate function

--- auto-empathie.js
+++ auto-empathie.js
@@ -13,35 +13,41 @@
      return doubleNumber(d.getHours()) + ":" + doubleNumber(d.getMinutes());
    }
 
-   function setup()
+   function handleMessage()
    {
      var inputBox = $("#inputbox");
-     inputBox.keypress(function(e) {
+     var text = inputBox.text();
+
+     if (!(/^\S/).test(text))
+       return;
+
+     var bubble = document.createElement("div");
+     bubble.className = "bubble you";
+     bubble.appendChild(document.createTextNode(text));
+
+     var timestamp = document.createElement("div");
+     var timestampText = createTimeString(new Date());
+     timestamp.appendChild(document.createTextNode(timestampText));
+     timestamp.className = "timestamp";
+     bubble.appendChild(timestamp);
+
+     $("#speech-container").append(bubble);
+
+     inputBox.empty();
+
+     var container = $("#speech-container")[0];
+     container.scrollTop = container.scrollHeight;
+   }
+
+   function setup()
+   {
+     $("#inputbox").keypress(function(e) {
        if (e.which != 13)
          return;
 
        e.preventDefault();
-       var text = inputBox.text();
 
-       if (!(/^\S/).test(text))
-         return;
-
-       var bubble = document.createElement("div");
-       bubble.className = "bubble you";
-       bubble.appendChild(document.createTextNode(text));
-
-       var timestamp = document.createElement("div");
-       var timestampText = createTimeString(new Date());
-       timestamp.appendChild(document.createTextNode(timestampText));
-       timestamp.className = "timestamp";
-       bubble.appendChild(timestamp);
-
-       $("#speech-container").append(bubble);
-
-       inputBox.empty();
-
-       var container = $("#speech-container")[0];
-       container.scrollTop = container.scrollHeight;
+       handleMessage();
      });
    }
 
